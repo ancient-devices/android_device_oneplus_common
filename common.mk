9b00366e55f66b5869f05951d2fb0713ff193bae
Sugakesshaa <sugakesshaa@gmail.com>
2023-04-26 00:08:19 +0800

    sm8350-common: Drop OplusDoze
    * in favour of our custom multihal service and aosp lift to wake service
    
    Change-Id: Id9fd5ccd64de97021bd63b633be111787d38570a

--- common.mk
+++ common.mk
@@ -155,10 +155,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
-
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
